7ff7d8db88fac2f71363584b8c9d299dadd17a12
alecoad <andrewlecoad@gmail.com>
2020-04-09 20:31:16 -0600

    Added convert time function, clear content function

--- static/index.js
+++ static/index.js
@@ -123,7 +123,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Store the message and time sent
             const message = document.querySelector('#message').value;
-            const time = Date.now();
+            const timestamp = Date.now();
+            const time = convertTime(timestamp);
 
             console.log(current_channel, message, sender, time);
 
@@ -230,7 +231,7 @@ document.addEventListener('DOMContentLoaded', () => {
             li.append(h5, h6, p);
             console.log('if worked');
         }
-        else 
+        else
             console.log('if did not work');
     });
 
@@ -238,15 +239,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Listen for logout event
     document.querySelector('#logout').onclick = () => {
-        // Remove name from local storage
+        // Remove name and channel from local storage
         localStorage.removeItem('name');
+        localStorage.removeItem('channel');
         console.log("logged out");
 
         // Unhide display name form and clear name
         document.querySelector('#name-form').style.display='block';
         document.querySelector('#name-display').innerHTML = 'DISPLAY NAME';
+
+        // Clear messages
+        clearContent('#message-list');
+        console.log('cleeeered');
+
+        location.reload();
     };
 });
 
+// Clear content function
+function clearContent(id) {
+    document.querySelector(id).innerHTML = '';
+}
+
 // Get current time function
-// TODO
+function convertTime(timestamp) {
+
+     // Get the current time
+     var now = new Date(timestamp);
+
+     // Create a list of months
+     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+     // Get the month, year, date, hour, minutes, and seconds
+     let month = months[now.getMonth()];
+     let date = now.getDate();
+     let year = now.getFullYear();
+     var hours = now.getHours();
+     var minutes = now.getMinutes();
+     var seconds = now.getSeconds();
+     var meridian = 'AM';
+
+     // Set Hours
+     if (hours >= 12){
+          meridian = 'PM';
+     }
+     if (hours > 12){
+          hours = hours - 12;
+     }
+
+     // Set Minutes
+     if (minutes < 10){
+          minutes = '0' + minutes;
+     }
+
+     // Set Seconds
+     if (seconds < 10){
+          seconds = '0' + seconds;
+     }
+
+     // Put together the time string
+     var time = month + ' ' + date + ', ' + year + ' | ' + hours + ":" + minutes + ":" + seconds + " " + meridian;
+     return time;
+}
